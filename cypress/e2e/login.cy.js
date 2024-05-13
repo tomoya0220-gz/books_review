3ec6da770e3894c794998d0688b50e0b18d64dcc
tomoya0220-gz <tomoya02201998@gmail.com>
2024-05-13 09:05:06 +0900

    Cypressが動くのか

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,19 +1,5 @@
-describe('Login API Test', () => {
-  it('should authenticate the user', () => {
-    const loginDetails = {
-      email: 'user@example.com',
-      password: 'yourPassword'
-    }
-
-    cy.request({
-      method: 'POST',
-      url: 'https://railway.bookreview.techtrain.dev/signin',
-      body: loginDetails,
-    headers: {
-      'Content-Type': 'application/json'
-    }
-    }).then((response) => {
-      expect(response.status).to.eq(200);
-    });
-  });
-});
+describe('Cypress', () =>{
+  it('が動いているか', () => {
+    expect(true).to.equal(true)
+  })
+})
