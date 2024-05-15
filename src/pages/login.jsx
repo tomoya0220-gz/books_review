6d906e59e2ba1a89d3b0c9e9fde09a9a823f1169
tomoya0220-gz <tomoya02201998@gmail.com>
2024-05-15 22:01:34 +0900

    ログイン画面の作成

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import './login.css';
+import { useCookies } from 'react-cookie';
+import { Navigate, useNavigate, Link } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import { url } from "../const";
+import { signIn } from '../authSlice';
 
 export const Login = () => {
+  const auth = useSelector((state) => state.auth.isSignIn);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState();
+  const [, setCookie] = useCookies(['token']);
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+  const onSignIn = (e) => {
+    e.preventDefault();
+    axios
+      .post(`${url}/signin`, { email: email, password: password })
+      .then((res) => {
+        setCookie('token', res.data.token);
+        dispatch(signIn());
+        navigate('/');
+      })
+      .catch((err) => {
+        const errorMessage = err.response && err.response.data ? err.response.data.message : 'ログインに失敗しました';
+        setErrorMessage(errorMessage);
+      });
+  };
 
-}
+  if (auth) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <>
+      <main className="signin">
+        <h2>ログイン</h2>
+        <p>{errorMessage}</p>
+        <form onSubmit={onSignIn}>
+          <label>メールアドレス</label>
+          <br />
+          <input 
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+          <br />
+          <label>パスワード</label>
+          <br />
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <br />
+          <button type="submit">
+            ログイン
+          </button>
+        </form>
+        <p>
+        ユーザー作成画面へ <Link to="/signup">ユーザー作成</Link>
+        </p>
+      </main>
+    </>
+  );
+};
